a3e28745fea05373f870705cdeb795b5291d5e31
Dinh Quoc Tri <tridq6280@ut.edu.vn>
2025-01-11 08:54:42 +0700

    thay doi

---
+++ STEMforFPT/fpt/home/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('courses/', views.courses, name='courses'),
+    path('contact/', views.contact, name='contact'),
+    path('tapdoan/', views.tapdoan, name='tapdoan'),
+    path('lichsu/', views.lichsu, name='lichsu'),
+    path('thongbao/', views.thongbao, name='thongbao'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
