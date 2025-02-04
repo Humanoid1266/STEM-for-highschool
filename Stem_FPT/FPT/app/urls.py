15d346ed47b77a1216d37ed1770378ff70aa363e
Lequangm <mylq8417@ut.edu.vn>
2025-02-04 19:58:26 +0700

    Add files via upload

--- Stem_FPT/FPT/app/urls.py
+++ Stem_FPT/FPT/app/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name ='home'),
+    path('tapdoan/', views.tapdoan, name='tapdoan'),
+    path('lichsu/', views.lichsu, name='lichsu'),
+    path('hethongfpt/', views.hethongfpt, name='hethongfpt'),
+    path('chuongtrinhdaotao/', views.chuongtrinhdaotao, name='chuongtrinhdaotao'),
+    path('congcuhoctap/', views.congcuhoctap, name='congcuhoctap'),
+    path('monhoc/', views.monhoc, name='monhoc'),
+    path('lienhe/', views.lienhe, name='lienhe'),
 ]
